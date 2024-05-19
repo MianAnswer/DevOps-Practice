7e2b081b5f26332f1af698ba8a1c070a09872437
MianAnswer <33139058+MianAnswer@users.noreply.github.com>
2024-05-19 01:03:27 -0400

    Setup backend (#9)
    * Setup CI
    
    * ignore node modules
    
    * initialize api
    
    * Set up back end
    
    * Test script for back end
    
    * setup CI
    
    * setup CI #3
    
    * install correct version of node
    
    * Linting errors resolved
    
    * Linting errors resolved
    
    * Linting errors resolved
    
    * Linting errors resolved
    
    * Linting errors resolved
    
    ---------
    
    Co-authored-by: MianAnswer <miananswer@hotmail.com>

---
+++ api/test/app.test.js
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import { expect } from 'chai'
+import app from '../app.js'
+
+describe('GET /', () => {
+  it('should return Hello, world!', (done) => {
+    request(app)
+      .get('/')
+      .end((err, res) => {
+        if (err) {
+          expect.fail(err)
+        } else {
+          expect(res.status).to.equal(200)
+          expect(res.text).to.equal('Hello, world!')
+        }
+        done()
+      })
+  })
+})
